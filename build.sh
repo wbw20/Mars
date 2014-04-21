05791f92a103a1ebfb9fdf3bfd60e9ff2e2fb113
William B. Wettersten <wbw20@case.edu>
2014-04-21 18:18:05 -0400

    a day in the life of a Java developer

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm dist/Mars.jar
 find . -name "*.class" | xargs rm -rf
-javac Mars.java
+javac -verbose $(find . | grep .java)
 jar cmf mainclass.txt dist/Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt Mars.java Mars.class docs help images mars 
