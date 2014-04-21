13de61cd8a32004307c8fe6db2fc215aadfb5a79
William B. Wettersten <wbw20@case.edu>
2014-04-21 17:42:04 -0400

    add clean step

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+find . -name "*.class" | xargs rm -rf
+javac Mars.java
 jar cmf mainclass.txt dist/Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt Mars.java Mars.class docs help images mars 
